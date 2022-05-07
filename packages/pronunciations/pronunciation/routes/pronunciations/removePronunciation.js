3d41015caa57891633c9a7173b05cc9293ba735b
Davi DeBarros <ddebarros@digitalocean.com>
2022-05-06 21:48:05 -0400

    debug

--- packages/pronunciations/pronunciation/routes/pronunciations/removePronunciation.js
+++ packages/pronunciations/pronunciation/routes/pronunciations/removePronunciation.js
@@ -14,12 +14,13 @@ async function removePronunciation(args) {
 
   try {
     const connection = await mysql;
+    console.log('attempting to delete, ', id, secret)
     const selectSql = `
       SELECT 
         BIN_TO_UUID(uuid) as uuid,
         secret
       FROM pronunciations
-      WHERE uuid = UUID_TO_BIN(?)
+      WHERE uuid = UUID_TO_BIN('?')
     `
 
     const [selectRow] = await connection.query(selectSql, [id]);
@@ -44,7 +45,7 @@ async function removePronunciation(args) {
 
     const deleteSql = `
       DELETE FROM pronunciations
-      WHERE uuid = UUID_TO_BIN(?) AND secret = ?
+      WHERE uuid = UUID_TO_BIN('?') AND secret = ?
     `;
 
     await connection.query(
